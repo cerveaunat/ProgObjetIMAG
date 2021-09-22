c495a2d306a961e8f0b12ca85f445e0c900c5b36
Nathan <Nathan.Cerveau@grenoble-inp.org>
2021-09-22 12:30:13 +0200

    partie II du TP

---
+++ Vector.java
@@ -0,0 +1,10 @@
+// TP numéro 2, création d'une classe "Vector", un tableau de rationnels
+// Date 22/9/21
+// author cerveaun 
+
+public class Vector {
+    Public Rational[] ()
+    {
+        
+    }
+}
