f424a2b8c282017b304e2f71fb6c192286e19313
Nathan <Nathan.Cerveau@grenoble-inp.org>
2021-09-22 12:22:58 +0200

    Second Commit

--- Rational.java
+++ Rational.java
@@ -17,12 +17,47 @@ class Rational
         this.denom = denom;
     }
 
-    public String toString() {
+    public String toString() 
+    {
         // String stringToRational = this.num + "/" + this.denom;
         // return stringToRational;
 
         return new String(this.num + "/" + this.denom);
     }
+
+    public Rational(int num2, int denom){
+        num = num2;
+        setDenom(denom);
+        int pgcdFixe = pgcd(denom,num); 
+        num = num/pgcdFixe;
+        setDenom(denom/pgcdFixe);
+    }
+
+    private int pgcd(int a, int b)
+    {
+        if (b==0)
+        {
+            return a;
+        }
+        return pgcd(b, a%b);
+    }
+
+    public void mult(Rational b)
+    {
+        int numB = b.num;
+        int denomB = b.getDenom();
+        this.num *= numB;
+        this.denom *= denomB;
+    }
+
+    public void add(Rational b)
+    {   int numB = b.num;
+        int denomB = b.getDenom();
+        this.num = this.num*denomB + this.denom*numB;
+        this.denom *= denomB;
+    }
+
+
 }
 
 
--- TestRational.java
+++ TestRational.java
@@ -3,9 +3,7 @@ public class TestRational
     public static void main(String [] args) 
     {
         Rational troisDemies = null;
-        troisDemies = new Rational();
-        troisDemies.num = 3;
-        troisDemies.setDenom(2);
+        troisDemies = new Rational(3,2);
 
         // Tests question 1 
 
@@ -15,13 +13,32 @@ public class TestRational
         
         System.out.println(troisDemies.toString());
 
-        // Tests question 3
+        // Tests question 3 et 4
 
-        Rational denomNul = null; 
-        denomNul = new Rational();
-        denomNul.num = 4;
-        denomNul.setDenom(0);
+        // Rational denomNul = null; 
+        // denomNul = new Rational(4,0);
+    
 
-        System.out.println(denomNul.toString());
+        // System.out.println(denomNul.toString());
+
+        // Tests question 5 
+
+        Rational unTiers = null;
+        unTiers = new Rational(1, 3);
+        unTiers.mult(troisDemies);
+        System.out.println(unTiers.toString());
+
+        // Tests question 6
+
+        Rational unQuart = null;
+        unQuart = new Rational(1,4);
+        unQuart.add(troisDemies);
+        System.out.println(unQuart.toString());
+
+        // Tests question 7
+
+        Rational deuxHuitiemes = null;
+        deuxHuitiemes = new Rational(2,8);
+        System.out.println(deuxHuitiemes.toString());
     }
 }
